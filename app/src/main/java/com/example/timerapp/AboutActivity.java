4931096c196280c2ff44c30b0c1f9a0078d4887f
Eganin <egorzaharin12@gmail.com>
2020-08-25 14:29:11 +0500

    add back button from settings

--- app/src/main/java/com/example/timerapp/AboutActivity.java
+++ app/src/main/java/com/example/timerapp/AboutActivity.java
@@ -7,12 +7,12 @@ import androidx.core.app.NavUtils;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_about);
 
         // переопределяем поведенте стрелки назад
         ActionBar actionBar = this.getSupportActionBar();
--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.timerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -206,6 +208,31 @@ public class MainActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.timer_menu,menu);
 
         return true;
+    }
+
+    /*
+    Метод срабатывет когда происходит нажатие на какой либо элемент
+    в меню
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Intent openSettings = new Intent(this , SettingsActivity.class);
+            startActivity(openSettings);
+            return true;
+        }else if(id == R.id.action_about){
+            Intent openAbout = new Intent(this , AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }else if(id == R.id.action_purchase){
+            Intent openPurchase = new Intent(this , PurshaseActivity.class);
+            startActivity(openPurchase);
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
