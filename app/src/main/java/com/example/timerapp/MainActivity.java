9d46f618c6418d06f2f4898653ef7b5a3fb9b319
Eganin <egorzaharin12@gmail.com>
2020-08-25 13:05:26 +0500

    refactor

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import static com.example.timerapp.MainActivity.endOfTime;
 
 public class MainActivity extends AppCompatActivity {
 
-    public  MediaPlayer mediaPlayer;
-    public  TextView textView;
-    public  final static String endOfTime = "00:10";
+    public MediaPlayer mediaPlayer;
+    public TextView textView;
+    public Button startButton;
     private final static String textStartTimer = "START";
     private final static String textStopTimer = "STOP";
+    private final static String textViewEndTimer = "00:00";
     private final static int interval = 1000;
     private final static int maxSecondsTimer = 3599;
-    private final static int defaultProgress = 60;
+    private final static int defaultProgress = 59;
     private SeekBar seekBar;
 
     @Override
@@ -31,19 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
         seekBar = findViewById(R.id.seekBar);
+        startButton = findViewById(R.id.startButton);
 
         seekBar.setMax(maxSecondsTimer);
         seekBar.setProgress(defaultProgress);
 
-        //AssertEndTime assertEndTime = new AssertEndTime();
-
-        //Thread myThread = new Thread(assertEndTime);
-        //myThread.start();
-
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                setTimerSeekBar(i);
+                setTimerSeekBar(i);// установка времени с помощью seekbar
             }
 
             @Override
@@ -60,30 +57,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setTimerSeekBar(int progress) {
+        /*
+        Данный метод устанавливает время с помощтю ползунка
+         */
         String resultTime = determinantOfTime(progress);
 
         textView.setText(resultTime);
     }
 
     private void roadSeekBar() {
+        /*
+        устанавливаем на seekbar текущее время
+         */
         seekBar.setProgress(getCurrentTime() - 1);
     }
 
 
     public void startTimer(View view) {
         // получаем текущее время
-        Integer currentTimeStart = getCurrentTime() * 1000;
+        startButton.setText(textStopTimer);
+        Integer currentTimeStart = getCurrentTime() * 1000;// получаем текущее время в милисекндах
         CountDownTimer myTimer = new CountDownTimer(currentTimeStart, interval) {
             @Override
             public void onTick(long l) {
-                String currentTime = determinantOfTime(getCurrentTime() - 1);
+                String currentTime = determinantOfTime(getCurrentTime());
+                Log.d("time",currentTime);
                 textView.setText(currentTime);
                 roadSeekBar();// сдвигаем seekBar назад по истечении метода
             }
 
             @Override
             public void onFinish() {
-                Log.d("timer", "End timer");
+                initMediaPlayer();
+                textView.setText(textViewEndTimer);
+                startButton.setText(textStartTimer);
+                playSound();
             }
         };
 
@@ -92,6 +100,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Integer getCurrentTime() {
+        /*
+        Метод возвоащает текущее воемя на таймере в секндах
+         */
         Integer currentTimeSecond =
                 Integer.parseInt(String.valueOf(textView.getText()).substring(3, 5));
         Integer currentTimeMinute =
@@ -101,40 +112,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String determinantOfTime(int totalTime) {
+        /*
+        Метод преобразовывает секундв в строку для вставки в TextView
+         */
         String settingTime = null;
         if (totalTime <= 60) {
-            settingTime = "00:" + totalTime;
+            if (totalTime < 10){
+                settingTime = "00:" +"0"+ totalTime;
+            }else{
+                settingTime = "00:" + totalTime;
+            }
         } else {
             String minute = String.valueOf(totalTime / 60);
             String second = String.valueOf(totalTime - (Integer.parseInt(minute) * 60));
-            if (minute.length() < 1) {
-                settingTime = "00:" + second;
-            } else {
-                if (minute.length() > 1) {
-                    if (Integer.parseInt(second) < 10) {
-                        settingTime = minute + ":" + "0" + second;
-                    } else {
-                        settingTime = minute + ":" + second;
-                    }
+
+            if (Integer.parseInt(minute) > 9) {
+                if (Integer.parseInt(second) < 10) {
+                    settingTime = minute + ":" + "0" + second;
                 } else {
-                    if (Integer.parseInt(second) < 10) {
-                        settingTime = "0" + minute + ":" + "0" + second;
-                    } else {
-                        settingTime = "0" + minute + ":" + second;
-                    }
+                    settingTime = minute + ":" + second;
+                }
+            } else {
+                if (Integer.parseInt(second) < 10) {
+                    settingTime = "0" + minute + ":" + "0" + second;
+                } else {
+                    settingTime = "0" + minute + ":" + second;
                 }
             }
         }
 
+
         return settingTime;
     }
 
-    public  void initMediaPlayer() {// инициализация плеера
+    public void initMediaPlayer() {// инициализация плеера
         mediaPlayer = MediaPlayer.create(getApplicationContext(),
                 R.raw.bell_sound);// указываем .mp3 mediaPlayer
     }
 
-    public void play() {
+    public void playSound() {
         mediaPlayer.start();// проигрываем музыку
     }
 
